b57f81f681af59fd1fa6f9871176d0535dd27acb
Koren Leslie Cohen <koren.cohen@gmail.com>
2015-07-09 10:39:17 -0400

    able to append random array item

--- public/javascripts/jcrew.js
+++ public/javascripts/jcrew.js
@@ -5,25 +5,38 @@ $(document).ready(function () {
   // preloader
   $('.preloader').delay(2500).fadeOut(500); 
 
-  var womenKnitArray = [],
-    womenSweaterArray = [];
+  // separate women's and men's tops/bottoms into arrays
+  var womenTopsArr = [],
+    womenBottomsArr = [],
+    menTopsArr = [],
+    menBottomsArr = [];
 
   $('.product').each(function (){
     var self = $(this),
       that = self.html();
 
-    if (that.includes("sweater")) {
-      womenSweaterArray.push(self.html());
-      console.log("sweater");
-      return womenSweaterArray;
-    } else if (that.includes("knit")) {
-      womenKnitArray.push(self.html());
-      console.log("knit");
-      return womenKnitArray;
+    if ((that.includes("womens_category/sweaters")) || (that.includes("womens_category/knitstees")) || (that.includes("womens_category/shirtsandtops2"))) {
+      womenTopsArr.push(that);
+      return womenTopsArr;
+    } else if ((that.includes("womens_category/pants")) || (that.includes("womens_category/skirts")) || (that.includes("womens_category/denim"))) {
+      womenBottomsArr.push(that);
+      return womenBottomsArr;
+    } else if ((that.includes("mens_category/shirts")) || (that.includes("mens_category/sweaters")) || (that.includes("mens_category/teespolos2"))) {
+      menTopsArr.push(that);
+      return menTopsArr;
+    } else if ((that.includes("mens_category/pants")) || (that.includes("mens_category/shorts"))) {
+      menBottomsArr.push(that);
+      return menBottomsArr;
     }
   });
 
-  console.log("womenSweaterArray outside: " + womenSweaterArray);
-  console.log("womenKnitArray outside: " + womenKnitArray);
+  console.log("womenTopsArr outside: " + womenTopsArr);
+  console.log("womenBottomsArr outside: " + womenBottomsArr);
+  console.log("menTopsArr outside: " + menTopsArr);
+  console.log("menBottomsArr outside: " + menBottomsArr);
+
+  $('.products').hide();
+
+  $('.women-tops').append(womenTopsArr[Math.floor(Math.random() * womenTopsArr.length)]);
 
 });
