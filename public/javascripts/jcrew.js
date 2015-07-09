2e09000c78f4ca496ffa09702f0bb9d216cc2d46
Koren Leslie Cohen <koren.cohen@gmail.com>
2015-07-08 23:44:08 -0400

    pulling elements from dom, commented out some scraping for speed

--- public/javascripts/jcrew.js
+++ public/javascripts/jcrew.js
@@ -5,5 +5,21 @@ $(document).ready(function () {
   $('.preloader').delay(2500).fadeOut(500); 
   console.log("inside jcrew.js");
 
+  var productArray = [];
+  console.log("one");
+
+  $('.product').each(function (){
+    var self = $(this);
+      // productArray = [];
+    productArray.push(self.html());
+    // console.log("self inside: " + self.html());
+    console.log("productArray inside: " + productArray);
+    return productArray;
+    console.log("two");
+  });
+
+  console.log("productArray outside: " + productArray);
+  console.log("three");
+
 
 });
